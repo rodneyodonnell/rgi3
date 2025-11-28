Fix initial eval calculation

src/rgi/rgizero/train.py
@@ -99,8 +99,9 @@ class Trainer:
 
         for split, loader in [("train", self.train_loader), ("val", self.val_loader)]:
             data_iter = iter(loader)
-            losses = torch.zeros(self.train_config.eval_iters)
-            for k in range(min(self.train_config.eval_iters, len(loader))):
+            eval_iters = min(self.train_config.eval_iters, len(loader))
+            losses = torch.zeros(eval_iters)
+            for k in range(eval_iters):
                 data_batch = next(data_iter)
                 with self.ctx:
                     logits, loss = self.model(*data_batch)